42c55d4a7942cd0de8fa5e49c9b62227a6ed67aa
tmg <tmg@clubtammy.info>
2021-06-27 00:24:35 -0500

    Create run-cache.sh

---
+++ run-cache.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://laravel.com/docs/8.x/deployment
+php composer.phar install --optimize-autoloader --no-dev
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
